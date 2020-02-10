ea9b2e60ceb2ab3d404bf5298a5d7e09f77c63fb
Fran Rom <franciscoromero@spotahome.com>
2020-02-10 11:11:14 +0100

    Add SearchBar to filter PokemonList

--- src/components/PokemonList/PokemonListContainer.js
+++ src/components/PokemonList/PokemonListContainer.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
 import axios from 'axios';
 
 import PokemonList from './PokemonList';
 
 const RESULTS_NUMBER = 50;
 const GET_POKEMONS_LIST_ENDPOINT = `https://pokeapi.co/api/v2/pokemon?limit=${RESULTS_NUMBER}`;
+const PLACEHOLDER = 'Search here...';
+const PAGE_TITLE = 'POKEMON LIST';
 
 const PokemonListContainer = () => {
-  const [data, setData] = useState([]);
+  const [originalData, setOriginalData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [hasError, setHasError] = useState(false);
+  const [searchText, setSearchText] = useState('');
+  const [filteredData, setFilteredData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,7 +24,7 @@ const PokemonListContainer = () => {
       try {
         const response = await axios(GET_POKEMONS_LIST_ENDPOINT);
 
-        setData(response.data.results);
+        setOriginalData(response.data.results);
       } catch (error) {
         setHasError(true);
       }
@@ -31,6 +35,15 @@ const PokemonListContainer = () => {
     fetchData();
   }, []);
 
+  const filterData = inputText => {
+    const results = originalData.filter(
+      item => item.name === inputText.toLowerCase()
+    );
+
+    setSearchText(inputText);
+    setFilteredData(results);
+  };
+
   return (
     <View>
       {hasError && <Text>Something was wrong...</Text>}
@@ -38,10 +51,41 @@ const PokemonListContainer = () => {
       {isLoading ? (
         <Text>Loading...</Text>
       ) : (
-        <PokemonList pokemonsData={data} />
+        <View style={styles.container}>
+          <Text style={styles.title}>{PAGE_TITLE}</Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder={PLACEHOLDER}
+            onChangeText={filterData}
+            value={searchText}
+          />
+          <PokemonList pokemonsData={filteredData || originalData} />
+        </View>
       )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1,
+    marginTop: 40,
+    padding: 16
+  },
+  title: {
+    fontSize: 30,
+    textAlign: 'center'
+  },
+  textInput: {
+    marginTop: 16,
+    marginBottom: 8,
+    height: 40,
+    borderWidth: 1,
+    paddingLeft: 10,
+    borderColor: '#009688',
+    backgroundColor: '#FFFFFF'
+  }
+});
 
 export default PokemonListContainer;
